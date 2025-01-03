1c3f458dffadd19bff5178cfe4ab2459a1ead42a
George Abesadze <abesadze.george@gmail.com>
2025-01-03 20:20:39 +0400

    extensions

---
+++ CS50P/extensions/extensions.py
@@ -0,0 +1,22 @@
+def main():
+    file = input("File name: ")
+    print(media_type(file))
+
+def media_type(f: str):
+    extension = f.split(".")[-1].lower().strip()
+
+    match extension:
+        case "gif" | "png":
+            return f"image/{extension}"
+        case "jpeg" | "jpg":
+            return "image/jpeg"
+        case "zip" | "pdf":
+            return f"application/{extension}"
+        case "txt":
+            return "text/plain"
+        case "bin":
+            return "application/octet-stream"
+        case _:
+            return "application/octet-stream"
+
+main()
